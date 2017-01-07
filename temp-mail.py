07ea0e93c684e7ba845706071597de7096eb307a
John Smith <v2.0@protonmail.com>
2017-01-07 15:23:22 +0500

    - support for both servers: api.temp-mail.org, api.temp-mail.ru - refactoring

--- temp-mail.py
+++ temp-mail.py
@@ -7,46 +7,65 @@ from urllib.parse import urlunparse
 
 
 CONST_API_SETTINGS = {
-    'api_url_scheme': 'https',
-    'api_domain': 'api.temp-mail',
+    'url_scheme': 'https',
+    'domain': 'api.temp-mail',
     'top_level_domains': ['org', 'ru']
 }
 
 
 class TempMail:
-    def __init__(self, login=None, domain=None, api_top_level_domain='org'):
-        self.api_top_level_domain = api_top_level_domain
-        if self.api_top_level_domain.lower() not in CONST_API_SETTINGS['top_level_domains']:
-            raise ValueError
-
+    def __init__(self, login=None, domain=None):
         self.login = login
         self.domain = domain
+
+    def __repr__(self):
+        return self.email
+
+    @property
+    def domain(self):
+        return self._domain
+    @domain.setter
+    def domain(self, domain):
+        if domain:
+            domain = domain.strip()
+            if not domain.startswith('@'):
+                domain = ''.join(('@', domain))
+            if domain not in sum(self.available_domains.values(), []):
+                raise ValueError('Domain not found in available domains')
+        else:
+            domain = random.choice(sum(self.available_domains.values(), []))
+
+        for api_tld, domains in self.available_domains.items():
+            if domain in domains:
+                self.api_top_level_domain = api_tld
+        self._domain = domain
+
+    @property
+    def login(self):
+        return self._login
+    @login.setter
+    def login(self, login):
+        self._login = self.random_login() if not login else login.strip()
+
+    @property
+    def email(self):
+        email = ''.join((self.login, self.domain))
+        setattr(self, '_email', email)
+        return self._email
 
     @property
     def available_domains(self):
         path = 'domains'
         if not hasattr(self, '_available_domains'):
-            domains = self.__request(path)
+            domains = {}
+            for val in CONST_API_SETTINGS['top_level_domains']:
+                self.api_top_level_domain = val
+                domains[val] = self.__request(path)
             setattr(self, '_available_domains', domains)
         return self._available_domains
 
-    @property
-    def email_address(self):
-        self.login = self.__random_login() if not self.login else self.login
-        self.domain = random.choice(self.available_domains) if not self.domain else self.domain
-
-        if not self.domain.startswith('@'):
-            self.domain = '@' + self.domain
-
-        if self.domain not in self.available_domains:
-            raise ValueError('Domain not found in available domains')
-
-        email = ''.join((self.login, self.domain))
-        setattr(self, '_email_address', email)
-        return self._email_address
-
     @staticmethod
-    def __random_login(min_len=6, max_len=20, digits=True):
+    def random_login(min_len=6, max_len=20, digits=True):
         chars = string.ascii_letters + string.digits if digits else string.ascii_letters
         return ''.join([random.choice(chars) for i in range(random.randint(min_len, max_len))])
 
@@ -57,14 +76,14 @@ class TempMail:
 
     def __request(self, path):
         response_format = 'format/json'
-        api_netloc = '.'.join((CONST_API_SETTINGS['api_domain'], self.api_top_level_domain))
+        api_netloc = '.'.join((CONST_API_SETTINGS['domain'], self.api_top_level_domain))
         path = '/'.join(('request', path, response_format))
-        url = urlunparse((CONST_API_SETTINGS['api_url_scheme'], api_netloc, path, '', '', ''))
+        url = urlunparse((CONST_API_SETTINGS['url_scheme'], api_netloc, path, '', '', ''))
         return requests.get(url).json()
 
     def get_mailbox(self):
         path = 'mail/id'
-        path = '/'.join((path, self.__md5_hash(self.email_address)))
+        path = '/'.join((path, self.__md5_hash(self.email)))
         return self.__request(path)
 
     def delete_message(self, msg_id):
